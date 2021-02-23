154911e6a16bf0cd707c443c2ac8b34761187754
edward <edward.yu0830@gmail.com>
2021-02-23 23:03:14 +0800

    2/23 測試錄音功能

---
+++ windows/d3dshot/audio_sender.py
@@ -0,0 +1,51 @@
+import pyaudio
+import wave
+import threading
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("* done recording")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+class AudioSender(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.CHUNK = 1024
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 2
+        self.RATE = 44100
+        self.RECORD_SECONDS = 5
+        self.WAVE_OUTPUT_FILENAME = "output.wav"
+    def run(self):
+        
--- windows/d3dshot/windows_sender.py
+++ windows/d3dshot/windows_sender.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import cv2
 import socket
 import time
