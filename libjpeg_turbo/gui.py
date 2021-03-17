e067339371d463df667709949ba0e8a2459c344a
Edward <edward.yu0830@gmail.com>
2021-03-17 23:23:58 +0800

    可以跑的版本 要做停止server的部分

--- libjpeg_turbo/gui.py
+++ libjpeg_turbo/gui.py
@@ -14,32 +14,37 @@ class Main(QMainWindow, ui.Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.Start_Button.clicked.connect(self.start_button_clicked)
+        self.Stop_Button.clicked.connect(self.stop_button_clicked)
         self.loadthread = None
+        self.cli_addr = []
 
     def start_button_clicked(self):
         self.logs.appendPlainText('start server.\nwaiting for request...')
-        self.loadthread = Worker(self)
+        self.loadthread = Worker(self.cli_addr, self)
         self.loadthread.start()
         self.loadthread.finished.connect(self.client_connected)
         self.Start_Button.setEnabled(False)
 
     def client_connected(self):
-        pass
-        # self.logs.appendPlainText(f'client[{self.worker.client_addr}] connected')
-        # 做完object被移除掉了
+        self.logs.appendPlainText(f'client[{self.cli_addr[0]}] connected')
+
+    def stop_button_clicked(self):
+        self.logs.appendPlainText('stop server')
 
 
 class Worker(QThread):
 
-    def __init__(self, parent=None):
+    def __init__(self,  ret: list, parent=None):
         QThread.__init__(self, parent=parent)
         self.server = sender.StartServer()
         self.client_addr = None
         self.server.start()
+        self.ret = ret
 
     def run(self):
         while self.client_addr is None:
             self.client_addr = self.server.get_addr()
+        self.ret.append(self.client_addr)
 
 
 if __name__ == "__main__":
