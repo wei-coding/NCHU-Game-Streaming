ea4a01b226a43b01ee705bda89f3ab0cb701b751
Edward <edward.yu0830@gmail.com>
2021-03-18 13:55:57 +0800

    在gui顯示當前區網ip

--- libjpeg_turbo/gui.py
+++ libjpeg_turbo/gui.py
@@ -14,20 +14,24 @@ class Main(QMainWindow, ui.Ui_MainWindow):
         self.Start_Button.clicked.connect(self.start_button_clicked)
         self.Stop_Button.clicked.connect(self.stop_button_clicked)
         self.server = sender.StartServer()
-        self.loadthread = None
+        self.server_thread = None
         self.cli_addr = []
+        self.ip = []
+        self.iptool = IpService(self.ip, self)
+        self.iptool.start()
+        self.iptool.finished.connect(self.show_my_ip)
 
     def start_button_clicked(self):
         self.logs.appendPlainText('start server.\nwaiting for request...')
         self.server.start()
-        self.loadthread = Worker(self.cli_addr, self.server, self)
-        self.loadthread.start()
-        self.loadthread.finished.connect(self.client_connected)
+        self.server_thread = ServerService(self.cli_addr, self.server, self)
+        self.server_thread.start()
+        self.server_thread.finished.connect(self.client_connected)
         self.Start_Button.setEnabled(False)
         self.Stop_Button.setEnabled(True)
 
     def client_connected(self):
-        self.logs.appendPlainText(f'client[{self.cli_addr[0]}] connected')
+        self.logs.appendPlainText(f'client ip:{self.cli_addr[0]} connected')
 
     def stop_button_clicked(self):
         self.logs.appendPlainText('stop server')
@@ -36,9 +40,12 @@ class Main(QMainWindow, ui.Ui_MainWindow):
         self.Start_Button.setEnabled(True)
         self.Stop_Button.setEnabled(False)
 
+    def show_my_ip(self):
+        self.logs.appendPlainText(f'server ip:{self.ip[0]}')
 
-class Worker(QThread):
-    def __init__(self,  ret: list, server, parent=None):
+
+class ServerService(QThread):
+    def __init__(self, ret: list, server, parent=None):
         QThread.__init__(self, parent=parent)
         self.server = sender.StartServer()
         self.client_addr = None
@@ -49,6 +56,27 @@ class Worker(QThread):
         while self.client_addr is None:
             self.client_addr = self.server.get_addr()
         self.ret.append(self.client_addr)
+
+
+class IpService(QThread):
+    def __init__(self, ret: list, parent=None):
+        QThread.__init__(self, parent=parent)
+        self.ret = ret
+        self.sock = None
+        self.ip = None
+
+    def run(self):
+        import socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            ip = s.getsockname()[0]
+        except Exception:
+            ip = '127.0.0.1'
+        finally:
+            s.close()
+        self.ret.append(ip)
 
 
 if __name__ == "__main__":
