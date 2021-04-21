87433567fda53a0b5adf9b33004a9a560489a80c
wei-coding <edward.yu0830@gmail.com>
2021-04-22 00:05:06 +0800

    清除沒用到的部份，將實驗室未上傳的部份完成

--- signal_control/client_basic_type.py
+++ signal_control/client_basic_type.py
@@ -19,19 +19,13 @@ print(clientMessage)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 print("connet success")
-ax = 0
-bx = 0
 
 
 def mouse_thread():
-    # from pynput.mouse import Listener
 
     def on_move(x, y):
         print('Pointer moved to {0}'.format((x, y)))
-        # tojson = {'0': 'mm', '1': x, '2': y}
-        # tojson = json.dumps(tojson)
         data = GSSPBody(GSSP.MOUSE, GSSP.M, x, y, GSSP.NO_BTN, 0)
-        # print(tojson)
         client.send(data)
 
     def on_click(x, y, button, pressed):
@@ -39,31 +33,16 @@ def mouse_thread():
             'Pressed' if pressed else 'Released',
             (x, y)))
         print(button)
-        # print(type(button),type(pressed))
-        # tojson = {'0': 'mp', '1': x, '2': y}
-        # tojson = json.dumps(tojson)
-        # print(tojson)
         data = GSSPBody(GSSP.MOUSE, GSSP.P, x, y, GSSP.NO_BTN, 0)
         client.send(data)
         if not pressed:
-            # Stop listener
-            # tojson = {'0': 'mr', '1': x, '2': y}
-            # print(tojson)
-            # tojson = json.dumps(tojson)
-            # print(tojson)
             data = GSSPBody(GSSP.MOUSE, GSSP.R, x, y, GSSP.NO_BTN, 0)
             client.send(data)
-            # return False
 
     def on_scroll(x, y, dx, dy):
         print('Scrolled ', x, y, dx, dy)
-        # tojson = {'0': 'ms', '1': dx, '2': dy}
-        # print(tojson)
-        # tojson = json.dumps(tojson)
-        # print(tojson)
-        data = GSSPBody(GSSP.MOUSE, GSSP.S, x, y, GSSP.NO_BTN, 0)
+        data = GSSPBody(GSSP.MOUSE, GSSP.S, dx, dy, GSSP.NO_BTN, 0)
         client.send(data)
-        # client.send(tojson.encode())
 
     # Collect events until released
     with Listener(
@@ -80,13 +59,8 @@ def mouse_thread():
 def keyboard_thread():
     def on_press(key):
         try:
-            print('alphanumeric key {0} pressed'.format(
-                key.char))
-
-            # a = {'0': 'kp', '1': key.char, '2': 5}
-            # b = json.dumps(a)
-            # print('inside', b)
-            data = GSSPBody(GSSP.KEYBOARD, GSSP.P, 0, 0, bytes(key.char), 0)
+            print('alphanumeric key {0} pressed'.format(key.char))
+            data = GSSPBody(GSSP.KEYBOARD, GSSP.P, 0, 0, bytes(key.char, encoding='utf-8'), 0)
             client.send(data)
 
         except AttributeError:
@@ -100,22 +74,14 @@ def keyboard_thread():
                 keyboard.Key.enter: GSSP.ENTER,
             }
 
-            data = GSSPBody(GSSP.KEYBOARD, GSSP.P, 0, 0, switch[key], 0)
-
-            # a={'0':'kp','1':key.char,'2':0}
-            # print(a)
-            # b = json.dumps(a)
-            # print(b)
+            data = GSSPBody(GSSP.KEYBOARD, GSSP.P, 0, 0, 0, switch[key])
             client.send(data)
-            # client.sendall(key)
 
     def on_release(key):
         try:
-            data = GSSPBody(GSSP.KEYBOARD, GSSP.R, 0, 0, bytes(key.char), 0, 0)
+            data = GSSPBody(GSSP.KEYBOARD, GSSP.R, 0, 0, bytes(key.char, encoding='utf-8'), 0)
             client.send(data)
             print('get', key.char)
-
-
         except Exception:
             if key == keyboard.Key.esc:
                 data = GSSPBody(GSSP.KEYBOARD, GSSP.R, 0, 0, GSSP.NO_BTN, GSSP.ESC)
@@ -130,11 +96,7 @@ def keyboard_thread():
                 keyboard.Key.right: GSSP.RIGHT,
                 keyboard.Key.enter: GSSP.ENTER,
             }
-
-            # a={'0':'kr','1':'a','2':0}
-            # b = json.dumps(a)
-            # print(b)
-            data = GSSPBody(GSSP.KEYBOARD, GSSP.R, 0, 0, bytes(switch[key]), 0)
+            data = GSSPBody(GSSP.KEYBOARD, GSSP.R, 0, 0, 0, bytes(switch[key]))
             client.send(data)
 
     # Collect events until released
