f4afced79b11d5ed8ba209802e7e53da076c8520
leonardolin <st950065@yahoo.com.tw>
2021-04-21 13:19:20 +0800

    try裡面放只放解碼的部分

--- signal_control/server_basic_type.py
+++ signal_control/server_basic_type.py
@@ -64,51 +64,52 @@ while True:
     try:
         clientMessage = (conn.recv(1024))
         signal=json.loads(clientMessage)
-        #print (clientMessage)
-        print(signal)
-        if signal['0']=='mm':
-            mmove(int(signal['1']),int(signal['2']))
-        elif signal['0']=='mp':
-            mpress()
-        elif signal['0']=='mr':
-            mrelease()
-        elif signal['0']=='ms':
-            mscroll(int(signal['1']), int(signal['2']))
-        elif signal['0']=='kp':
-            if signal['1']=='up':
-                keyboard_control.press(keyboard.Key.up)       
-            elif signal['1']=='down':
-                keyboard_control.press(keyboard.Key.down)         
-            elif signal['1']=='left':
-                keyboard_control.press(keyboard.Key.left)
-                #pressing('left')
-            elif signal['1']=='right':
-                keyboard_control.press(keyboard.Key.right)
-                #pressing('right')
-            elif signal['1']=='enter':
-                keyboard_control.press(keyboard.Key.enter)
-                #pressing('enter')
-            else :   
-                kpress(signal['1'])
-            #kpress(signal['1'])            
-        elif signal['0']=='kr':
-            if signal['1']=='up':
-                keyboard_control.release(keyboard.Key.up)
-                #check_if_press.remove('up')
-            elif signal['1']=='down':
-                keyboard_control.release(keyboard.Key.down)
-                #check_if_press.remove('down')
-            elif signal['1']=='left':
-                keyboard_control.release(keyboard.Key.left)
-                #check_if_press.remove('left')
-            elif signal['1']=='right':
-                keyboard_control.release(keyboard.Key.right)
-                #check_if_press.remove('right')
-            elif signal['1']=='enter':
-                keyboard_control.release(keyboard.Key.enter)
-                #check_if_press.remove('enter')
-            else:
-                krelease((signal['1']))
     except:
-        pass
+        continue
+        #print (clientMessage)
+    print(signal)
+    if signal['0']=='mm':
+        mmove(int(signal['1']),int(signal['2']))
+    elif signal['0']=='mp':
+        mpress()
+    elif signal['0']=='mr':
+        mrelease()
+    elif signal['0']=='ms':
+        mscroll(int(signal['1']), int(signal['2']))
+    elif signal['0']=='kp':
+        if signal['1']=='up':
+            keyboard_control.press(keyboard.Key.up)       
+        elif signal['1']=='down':
+            keyboard_control.press(keyboard.Key.down)         
+        elif signal['1']=='left':
+            keyboard_control.press(keyboard.Key.left)
+            #pressing('left')
+        elif signal['1']=='right':
+            keyboard_control.press(keyboard.Key.right)
+            #pressing('right')
+        elif signal['1']=='enter':
+            keyboard_control.press(keyboard.Key.enter)
+            #pressing('enter')
+        else :   
+            kpress(signal['1'])
+        #kpress(signal['1'])            
+    elif signal['0']=='kr':
+        if signal['1']=='up':
+            keyboard_control.release(keyboard.Key.up)
+            #check_if_press.remove('up')
+        elif signal['1']=='down':
+            keyboard_control.release(keyboard.Key.down)
+            #check_if_press.remove('down')
+        elif signal['1']=='left':
+            keyboard_control.release(keyboard.Key.left)
+            #check_if_press.remove('left')
+        elif signal['1']=='right':
+            keyboard_control.release(keyboard.Key.right)
+            #check_if_press.remove('right')
+        elif signal['1']=='enter':
+            keyboard_control.release(keyboard.Key.enter)
+            #check_if_press.remove('enter')
+        else:
+            krelease((signal['1']))
+    
          
