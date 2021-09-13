ce039312fa27b4d462f33463da772164a5903659
wei-coding <edward.yu0830@gmail.com>
2021-09-13 10:37:12 +0800

    最後結構變更

--- ffmpeg/test.py
+++
@@ -1,2 +0,0 @@
-import ffmpeg_streaming
-capture = ffmpeg_streaming.input('CAMERA NAME OR SCREEN NAME', capture=True)
--- flask/server.py
+++
@@ -1,4 +0,0 @@
-from waitress import serve
-import app
-if __name__ == '__main__':
-    serve(app.app, host='0.0.0.0', port=8080)
